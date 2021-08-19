bc130e59dea19a0f9248e76c54f4ceefd6a87171
MikuroXina <ryosukadnak@gmail.com>
2021-08-19 23:15:41 +0900

    feat: Add ttf

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,44 @@
+//! # rich-sdl2-ttf-rust
+//!
+//! The rich-sdl2-ttf-rust provides wrapper for SDL2_ttf and abstractions of font rendering APIs.
+
+#![warn(missing_docs)]
+
+use rich_sdl2_rust::Sdl;
+use static_assertions::assert_not_impl_all;
+use std::{cell::Cell, marker::PhantomData};
+
 mod bind;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+/// A root SDL2_ttf controller.
+#[derive(Debug)]
+pub struct Ttf {
+    _phantom: PhantomData<Cell<u8>>,
+}
+
+assert_not_impl_all!(Ttf: Send, Sync);
+
+impl Ttf {
+    /// Constructs a root controller.
+    pub fn new() -> Self {
+        let ret = unsafe { bind::TTF_Init() };
+        if ret == -1 {
+            Sdl::error_then_panic("Ttf");
+        }
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl Drop for Ttf {
+    fn drop(&mut self) {
+        unsafe { bind::TTF_Quit() }
+    }
+}
+
+impl Default for Ttf {
+    fn default() -> Self {
+        Self::new()
     }
 }
